150792eb5d875bb6225e5d4b7b41069d08b96472
Michael Gorman <michael@michaeljgorman.com>
2014-11-15 21:54:07 -0800

    Removed comments

--- vocagram/urls.py
+++ vocagram/urls.py
@@ -4,10 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'vocagram.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^$', include('frontend.urls', namespace="frontend")),
     url(r'^admin/', include(admin.site.urls)),
 )
