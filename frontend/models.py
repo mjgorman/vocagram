7e40ed8e2ee59c0f09c64e32279de58d2c738ed0
Michael Gorman <michael@michaeljgorman.com>
2014-11-16 09:53:01 -0800

    Adding docstrings, quieting pylint

--- frontend/models.py
+++ frontend/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
+    """
+    This is the Post model, this is how things will be shared by users.
+    """
     user = models.CharField(max_length=16)
     title = models.CharField(max_length=140)
     body = models.TextField() # This will become audio file
--- frontend/views.py
+++ frontend/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
+    """
+    The main page, will end up being a timeline like page
+    """
     name = "Jennifer"
     html = "<html><body> Hi %s. This is the index page.</body></html>" % name
     return HttpResponse(html)
